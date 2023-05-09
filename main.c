d2a55cdfd1068678519a2426ae59f5d2c8229080
Kathryn6368 <6368@holbertonstudents.com>
2023-05-09 00:16:46 -0700

    get function pointer working

--- main.c
+++ main.c
@@ -4,6 +4,10 @@ void check_arguments(int argc);
 FILE *x_fopen(char *filename);
 char *my_getline(FILE *fp);
 char *get_opcode(char *text_line);
+void (*get_op_func(char *str))(stack_t **, unsigned int);
+void print_error(stack_t **stack, unsigned int line_number);
+void push_func(stack_t **stack, unsigned int line_number);
+void print_all(stack_t **stack, unsigned int line_number);
 
 int global_argument;
 
@@ -23,15 +27,21 @@ int main(int argc, char *argv[])
 	ssize_t bytes_read;
 	char *buffer = NULL;
 	global_argument = 0;
+	stack_t **stack;
 
 	check_arguments(argc);
 	fp = x_fopen(argv[1]);
 	bytes_read = getline(&buffer, &n, fp);
+	line_number = 1;
 	while (bytes_read >= 0)
 	{
 		opcode = get_opcode(buffer);
-		//get_op_func(opcode)(XXstack, line_number);
-		free(opcode);
+		if (opcode != NULL)
+		{
+			get_op_func(opcode)(stack, line_number);
+			free(opcode);
+		}
+		line_number = line_number + 1;
 		bytes_read = getline(&buffer, &n, fp);
 	}
 	free(buffer);
@@ -77,32 +87,6 @@ FILE *x_fopen(char *filename)
 }
 
 /**
- * my_getline - short description
- * @argument: a unsigned int *data type variable
- * @line_number: a unsigned int *data type variable
- *
- * Return: type is char *
- */
-
-/*char *my_getline(FILE *fp)
-{
-	char *line = NULL;
-	char *buffer = NULL;
-	ssize_t getline_return = 0;
-	size_t n = 0;
-
-	getline_return = getline(&buffer, &n, fp);
-	if (getline_return == -1)
-	{
-		free(buffer);
-		return (NULL);
-	}
-	line = strdup(buffer);
-	free(buffer);
-	return (line);
-}*/
-
-/**
  * get_opcode - short description
  * @text_line: a char *data type variable
  *
@@ -129,4 +113,70 @@ char *get_opcode(char *text_line)
 	}
 	printf("return value: %s / global_arg: %d\n", return_value, global_argument);
 	return (return_value);
+}
+
+/**
+ * get_op_func - pointer to a function that will match the opcode
+ * @str: opcode
+ *
+ * Return: void
+ */
+
+void (*get_op_func(char *str))(stack_t **, unsigned int)
+{
+	instruction_t ops[] = {
+		{"push", push_func},
+		{"pall", print_all},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].opcode != NULL)
+	{
+		if (strcmp(ops[i].opcode, str) == 0)
+		{
+			return (ops[i].f);
+		}
+		i = i + 1;
+	}
+	return print_error;
+}
+
+/**
+ * print_error - print error message
+ * @stack: double pointer to head of doubly linked list stack
+ * @line_number: unsigned integer
+ *
+ * Return: void
+ */
+void print_error(stack_t **stack, unsigned int line_number)
+{
+	fprintf(stderr, "L%ud: unknown instruction <opcode>\n", line_number);
+}
+
+/**
+ * push_func - short description
+ * @stack: a stack_t **data type variable
+ * @line_number: a unsigned int data type variable
+ *
+ * Return: type is void
+ */
+
+void push_func(stack_t **stack, unsigned int line_number)
+{
+	printf("function to push integer %d to stack\n", global_argument);
+}
+
+/**
+ * print_all - short description
+ * @stack: a stack_t **data type variable
+ * @line_number: a unsigned int data type variable
+ *
+ * Return: type is void
+ */
+
+void print_all(stack_t **stack, unsigned int line_number)
+{
+	printf("function to print all data in the stack\n");
 }
