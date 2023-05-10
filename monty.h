65269c66d18aaa8fe0d4e159fe868c8322ab4258
Kathryn6368 <6368@holbertonstudents.com>
2023-05-10 16:17:59 -0700

    small typos

--- monty.h
+++ monty.h
@@ -5,8 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <dirent.h>
 #include <stdarg.h>
 #include <ctype.h>
