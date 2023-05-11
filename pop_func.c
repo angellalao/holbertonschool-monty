c9ab4d92c6b81fd9960592fc572912b10f1e774a
angellalao <6369@holbertonstudents.com>
2023-05-11 03:29:08 -0700

    shorten variable name

--- pop_func.c
+++ pop_func.c
@@ -3,17 +3,17 @@
 /**
  * pop_func - short description
  * @stack: a stack_t **data type variable
- * @line_number: a unsigned int data type variable
+ * @line_num: a unsigned int data type variable
  *
  * Return: type is void
  */
-void pop_func(stack_t **stack, unsigned int line_number)
+void pop_func(stack_t **stack, unsigned int line_num)
 {
 	stack_t *temp;
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_num);
 		exit(EXIT_FAILURE);
 	}
 	temp = *stack;
