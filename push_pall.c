d2f241359be2d4f687e285c67a3ef108f534380a
angellalao <6369@holbertonstudents.com>
2023-05-11 03:13:13 -0700

    separated helper functions from main.c

---
+++ push_pall.c
@@ -0,0 +1,77 @@
+#include "monty.h"
+
+/**
+ * push_func - short description
+ * @stack: a stack_t **data type variable
+ * @line_number: a unsigned int data type variable
+ *
+ * Return: type is void
+ */
+void push_func(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+	stack_t *newNode;
+
+	newNode = malloc(sizeof(*newNode));
+	if (newNode == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	newNode->n = g_var.arg;
+	newNode->prev = NULL;
+	newNode->next = *stack;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = newNode;
+	}
+	*stack = newNode;
+}
+
+/**
+ * check_digit - short description
+ * @str: a char *data type variable
+ *
+ * Return: 1 if string is made of digits, 0 if not
+ */
+int check_digit(char *str)
+{
+	int i;
+
+	i = 0;
+	if (str[i] == '-')
+	{
+		i = 1;
+	}
+	while (str[i] != '\0')
+	{
+		if (isdigit(str[i]) == 0)
+		{
+			return (0);
+		}
+		i = i + 1;
+	}
+	return (1);
+}
+
+/**
+ * print_all - short description
+ * @stack: a stack_t **data type variable
+ * @line_number: a unsigned int data type variable
+ *
+ * Return: type is void
+ */
+void print_all(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL)
+	{
+		return;
+	}
+	temp = *stack;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
